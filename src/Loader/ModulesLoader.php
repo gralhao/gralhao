493db005cf68bcedfa080b9bf832e4adfd4ef688
Luã Mota <hi@mocallu.com>
2020-08-02 11:47:04 -0300

    Add banner files

--- src/Loader/ModulesLoader.php
+++ src/Loader/ModulesLoader.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @see       https://github.com/gralhao/gralhao
+ * @copyright https://github.com/gralhao/gralhao/blob/master/COPYRIGHT.md
+ * @license   https://github.com/gralhao/gralhao/blob/master/LICENSE.md
+ */
+
 declare(strict_types=1);
 
 namespace Gralhao\Loader;
